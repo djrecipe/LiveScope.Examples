7491cb3eb7fc954d649f5ed59d3aeeaf9ae9058b
djrecipe <brkfstmnchr@gmail.com>
2025-05-19 22:32:45 +0700

    start implementing MyDataCapture
    start implementing MyDataCapture

---
+++ LiveScope.Intergration.Examples/MyDataCapture.cs
@@ -0,0 +1,30 @@
+﻿namespace LiveScope.Intergration.Examples
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class MyDataCapture : IDataCapture
+    {
+        public void SetOnDataAvailable(SamplesDelegate callback)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartCapture()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopCapture()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int BufferSize { get; }
+        public DataSources DataSource { get; }
+        public Guid ID { get; }
+        public double SampleInterval { get; }
+        public SampleOrderTypes SampleOrder { get; }
+        public SweepModes SweepMode { get; }
+    }
+}
