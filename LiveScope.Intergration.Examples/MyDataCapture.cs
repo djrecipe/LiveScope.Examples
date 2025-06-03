e34fc99cd7a82e451da6303d2ca0d50fa3cc5afd
djrecipe <brkfstmnchr@gmail.com>
2025-06-03 14:10:46 +0700

    some comments
    some comments

--- LiveScope.Intergration.Examples/MyDataCapture.cs
+++ LiveScope.Intergration.Examples/MyDataCapture.cs
@@ -7,16 +7,25 @@
     {
         public void SetOnDataAvailable(SamplesDelegate callback)
         {
+            /*
+             * Set the callback to be called when data is available
+             */
             throw new NotImplementedException();
         }
 
         public void StartCapture()
         {
+            /*
+             * Send signals to hardware to initiate capture
+             */
             throw new NotImplementedException();
         }
 
         public void StopCapture()
         {
+            /*
+             * Send signals to hardware to halt capture
+             */
             throw new NotImplementedException();
         }
 
